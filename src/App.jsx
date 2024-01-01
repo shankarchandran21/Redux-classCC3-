0eb594a702d8ea78e7bcd4764c683f9bfbf42c0d
Shankar <shankarchandran21@gmail.com>
2024-01-01 21:59:06 +0530

    redux

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+
+
+
+function App() {
+
+
+ 
+
+  // if (isLoading) {
+  //   return (
+  //     <div className='loading'>
+  //       <h1>Loading...</h1>
+  //     </div>
+  //   );
+  // }
+
+  return (
+    <main>
+    
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+export default App;
